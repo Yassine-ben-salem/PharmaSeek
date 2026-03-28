064fb258cee08d450607b8181d4b4a2ac641e122
Yassine <medyassinebnsalem@gmail.com>
2026-03-28 11:47:03 +0100

    Remove redundant files

--- src/main/java/dtos/ClientDto.java
+++
@@ -1,4 +0,0 @@
-package dtos;
-
-public record ClientDto(Long id, String name,String email, String phone) {
-}
--- src/main/java/repositories/PharmacyRepository.java
+++
@@ -1,8 +0,0 @@
-package repositories;
-
-import entities.Pharmacy;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PharmacyRepository extends JpaRepository<Pharmacy, Long> {
-    boolean existsByTaxId(String taxId);
-}
