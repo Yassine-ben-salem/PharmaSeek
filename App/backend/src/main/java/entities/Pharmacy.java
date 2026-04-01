26881fa9e6b15c713c5675f7adb1b94cc984f3d9
Yassine <medyassinebnsalem@gmail.com>
2026-04-01 11:53:55 +0100

    Deleted Cattalog tables, Added db.sql in Plan

---
+++ App/backend/src/main/java/entities/Pharmacy.java
@@ -0,0 +1,55 @@
+package entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "pharmacy")
+public class Pharmacy {
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @MapsId
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "id")
+    private User user;
+
+
+    @Column(name = "pharmacy_name")
+    private String pharmacyName;
+
+
+    @Column(name = "tax_id")
+    private String taxId;
+
+    @Column(name ="email")
+    private String email;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "latitude")
+    private BigDecimal latitude;
+
+
+    @Column(name = "longitude")
+    private BigDecimal longitude;
+
+
+    @Column(name = "schedule")
+    private String schedule;
+
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+}
