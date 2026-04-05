8c86764da8c83dc7b1550d19f263264f089a62bf
Yassine <medyassinebnsalem@gmail.com>
2026-04-05 14:41:47 +0100

    Update backend with latest changes

--- App/backend/src/main/java/repositories/DrugRepository.java
+++ App/backend/src/main/java/repositories/DrugRepository.java
@@ -8,6 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface DrugRepository extends JpaRepository<Drug, Long> {
-    Optional<Drug> findByName(String name);
-
+    Optional<Drug> findByNameIgnoreCase(String name);
 }
